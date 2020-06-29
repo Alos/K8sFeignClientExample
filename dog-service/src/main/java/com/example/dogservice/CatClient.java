33409903f2cae4a6bfc1862f2f4d3c1d4f7c43fe
Sergio Felix <sfelix@google.com>
2020-06-29 13:34:37 -0700

    Added circuitbreaker with resilience4j and metrics.

--- dog-service/src/main/java/com/example/dogservice/CatClient.java
+++ dog-service/src/main/java/com/example/dogservice/CatClient.java
@@ -1,12 +1,19 @@
 package com.example.dogservice;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient("cat-service")
+@CircuitBreaker(name = "cat-service")
+@Retry(name = "cat-service")
 public interface CatClient {
 
   @GetMapping("/api/v1/cats")
   List<Cat> getAllCats();
+
+  @GetMapping("/api/v1/cats2")
+  List<Cat> getAllCats2();
 }
--- dog-service/src/main/java/com/example/dogservice/DogServiceApplication.java
+++ dog-service/src/main/java/com/example/dogservice/DogServiceApplication.java
@@ -1,12 +1,32 @@
 package com.example.dogservice;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import java.time.Duration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
 public class DogServiceApplication {
+
+  @Bean
+  public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
+
+    return factory ->
+        factory.configureDefault(
+            id ->
+                new Resilience4JConfigBuilder(id)
+                    .timeLimiterConfig(
+                        TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build())
+                    .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+                    .build());
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(DogServiceApplication.class, args);
